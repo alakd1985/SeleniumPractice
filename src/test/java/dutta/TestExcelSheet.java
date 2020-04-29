895dab733a64f6741f4594d2f46e45cfe9a9a51b
Alak Kanti Dutta <alakd1985@gmail.com>
2020-04-29 16:42:05 -0400

    Uploading Excell sheet to Github

---
+++ src/test/java/dutta/TestExcelSheet.java
@@ -0,0 +1,36 @@
+package dutta;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class TestExcelSheet {
+	
+	
+
+	public static void main(String[] args) throws IOException {
+		String path = "Data\\ExcelXLSX.xlsx";
+		FileInputStream fis = new FileInputStream(path);
+		Workbook book = new XSSFWorkbook(fis);
+		Sheet sheet=book.getSheetAt(0);
+		Row row= sheet.getRow(1);
+		Cell cell = row.getCell(1);
+		String cellvalue = cell.getStringCellValue();
+		System.out.println(cellvalue);
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
---
+++ src/test/java/dutta/TestPrintAllData.java
@@ -0,0 +1,42 @@
+package dutta;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class TestPrintAllData {
+
+	public static void main(String[] args) {
+		String path = "Data\\ExcelXLSX.xlsx";
+		try {
+			FileInputStream fis = new FileInputStream(path);
+			Workbook bWorkbook = new XSSFWorkbook();
+			Sheet sheet = bWorkbook.getSheetAt(0);
+			int lastrow = sheet.getLastRowNum();
+			System.out.println("The last row is : " + lastrow);
+
+			for (int i = 0; i <= lastrow; i++) {
+				Row row = sheet.getRow(i);
+				int lastcell = row.getLastCellNum();
+				for (int j = 0; j <= lastcell; j++) {
+					Cell cell = row.getCell(j);
+					String cellValue = cell.getStringCellValue();
+					System.out.println("The cellvalue is : " + cellValue);
+
+				}
+				System.out.println();
+			}
+
+		} catch (FileNotFoundException e) {
+
+			e.printStackTrace();
+		}
+
+	}
+
+}
